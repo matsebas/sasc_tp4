f56b74279e2c7819761e959f6ba23236f6c17518
Matias Sebastiao <msebastiao@max.capital>
2024-07-09 23:53:15 -0300

    persistencia conectada

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -3,8 +3,8 @@ import { date, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 export const eventos = pgTable('eventos', {
   id: serial('id').primaryKey(),
   text: varchar('text', { length: 50 }).notNull(),
-  start: date('start').notNull(),
-  end: date('end').notNull(),
-  backColor: varchar('backColor', { length: 50 }),
-  borderColor: varchar('borderColor', { length: 50 })
+  start: date('inicio').notNull(),
+  end: date('fin').notNull(),
+  backColor: varchar('backcolor', { length: 50 }),
+  borderColor: varchar('bordercolor', { length: 50 })
 });
